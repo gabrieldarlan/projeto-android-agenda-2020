9616f13a048d832f11f309ca5c360f1c88895f45
gabrieldarlan <gabriel.darlan88@gmail.com>
2020-02-02 18:44:30 -0300

    Segunda tentativa Commit

--- app/src/main/java/br/com/alura/agenda/MainActivity.java
+++ app/src/main/java/br/com/alura/agenda/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends Activity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+    //teste
     }
 }
